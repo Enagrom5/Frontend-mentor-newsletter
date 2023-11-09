3d57839e1889aa6e1c4803514cbaf93648fe8903
MORGANE DEBARGE <morganedebarge@msn.com>
2023-11-09 13:55:35 +0100

    moitié fait

---
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".button");
+const formulaire = document.querySelector(".formulaire");
+const afterSubmit = document.querySelector(".aftersubmit");
+const img = document.querySelector("img");
+const dismiss=document.createElement("button")
+dismiss.classList.add("dismiss")
+dismiss.innerText="Dismiss Message"
+afterSubmit.appendChild(dismiss)
+
+button.addEventListener("click", function () {
+  event.preventDefault();
+  afterSubmit.style.display = "flex";
+  formulaire.style.display = "none";
+  img.style.display="none"
+  
+});
+
+
+
