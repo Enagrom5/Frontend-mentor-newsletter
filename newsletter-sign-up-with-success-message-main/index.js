ce18a2ff25f999595c081d03e9cb66100f29e5e6
MORGANE DEBARGE <morganedebarge@msn.com>
2023-11-14 15:49:29 +0100

    ajout fonctionnalité adresse mail

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -2,19 +2,30 @@ const button = document.querySelector(".button");
 const formulaire = document.querySelector(".formulaire");
 const afterSubmit = document.querySelector(".aftersubmit");
 const img = document.querySelector("img");
-const dismiss=document.createElement("button");
+const dismiss = document.createElement("button");
+const email = document.querySelector("input");
+const label = document.querySelector("label");
 
 dismiss.classList.add("dismiss");
-dismiss.innerText="Dismiss Message";
+dismiss.innerText = "Dismiss Message";
 afterSubmit.appendChild(dismiss);
 
 button.addEventListener("click", function () {
   event.preventDefault();
   afterSubmit.style.display = "flex";
   formulaire.style.display = "none";
-  img.style.display="none"
-  
+  img.style.display = "none";
 });
-
-
+email.addEventListener("change", function (e) {
+  email.value = e.target.value;
+  if (email.value.includes("#") === true) {
+    const error=document.createElement("p");
+    label.appendChild(error);
+    error.innerText="Valid email required";
+    error.classList.add("error")
+    email.style.backgroundColor = "rgba(255, 99, 71, 0.564)";
+    email.style.color="white"
+  }
+ 
+});
 
